1b407ae4ff7ea035fe6c2123568ef37a9a649d1c
kang-seongbeom <56289258+kang-seongbeom@users.noreply.github.com>
2021-03-11 17:48:27 +0900

    js기본 1_1

--- app.js
+++ app.js
@@ -1,1 +1,13 @@
 console.log('app.js');
+
+var naming = "2017108243 강성범";
+var count = 2;
+var isMan = true;
+//한줄 주석
+/*여러줄 주석*/
+console.log(naming,count,isMan);
+var log= function log(str){
+    console.log(str);
+}
+
+log("hello");
