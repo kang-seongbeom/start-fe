1190c604a20a666f03237bff3987bd5153bcb524
kang-seongbeom <56289258+kang-seongbeom@users.noreply.github.com>
2021-03-18 19:34:35 +0900

    js 기본2_1, js 기본2_2

--- 03/app.js
+++ 03/app.js
@@ -86,12 +86,114 @@
 // byReference(count);
 // console.log(count);
 
-var isMan = false;
-var isHero=true;
-if(isMan){
-    console.log(1);
-}else if((!isMan) && (isHero)){
-    console.log(0);
-}else{
-    console.log(2);
-}
+// var isMan = false;
+// var isHero=true;
+// if(isMan){
+//     console.log(1);
+// }else if((!isMan) && (isHero)){
+//     console.log(0);
+// }else{
+//     console.log(2);
+// }
+
+// for(var i=1; i<10; i++){
+//     console.log(i,'단')
+//     for(var j=1; j<10; j++){
+//         console.log(i,'*',j,'=',i*j);
+//     }
+//     console.log("--------------");
+// }
+
+//alert창에 edit text 추가
+// var who = prompt("넌 누구냐?");
+// console.log(who);
+
+//alert창에 확인, 취소 버튼
+// var isDelete = confirm('진짜 삭제?');
+// console.log(isDelete);
+
+//숫자인지 판단. 숫자이면 false
+// console.log(isNaN('1'));
+
+// function test(){
+//     console.log("time");
+// }
+
+//1초후 1번
+//var id = setTimeout(test(), 1000);
+
+//1초후마다 clear 호출까지
+// var id = setInterval(test(), 1000);
+
+//호스팅
+// var name = '강성범';
+// function test(){
+//     console.log(name); //왜 undifined이지?
+//     var name = '2017108243';
+//     console.log(name);
+// }
+// test();
+
+// test2();
+// function test2(){
+//     console.log("test2");
+// }
+
+// function sum(a, b){
+//     return a+b;
+// }
+// console(sum(1,2));
+
+// var car = {
+//     year : 2021,
+//     starting : function(){
+//         console.log(this.year);
+//     },
+// };
+// car.starting();
+
+//즉시실행. 익명 클래스와 비슷. 캡슐화
+// (function(){
+//         var a='a';
+//     alert(a);
+// })();
+
+// function sum(x,y){
+//     if(!y) y=1;
+//     console.log(x+y);
+// }
+// sum(3); // 4
+// sum(3,3); //6
+
+//가변 길이 전달 인자
+// function sum(){
+//     var sum = 0;
+//     for (var i = 0; i<arguments.length; i++){
+//         sum += arguments[i];
+//     }
+//     console.log(sum);
+// }
+// sum(1,2,3,4,5,6,7,8,9,10);
+
+//정규식 Regx
+
+//test
+//true, fasle로 반환
+// var text = 'abc def ghi';
+// var testRegx = /abc1/;
+// var result = testRegx.test(text);
+// console.log(result);
+
+//match
+//객체와 null로 반환
+// var text = 'abc def ghi';
+// var matchRegx = /abc/;
+// var result = text.match(matchRegx);
+// console.log(result);
+
+//replace
+//대체 되거나 대체 되지 않거나
+// var text = 'abc def ghi';
+// var replaceRegx = /abc1/;
+// var result = text.replace(replaceRegx, 'zzz');
+// console.log(result);
