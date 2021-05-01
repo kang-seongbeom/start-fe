a0f0f5bd3683ff832c172be55556a0a34d96267f
kang-seongbeom <56289258+kang-seongbeom@users.noreply.github.com>
2021-05-01 21:15:20 +0900

    주석추가

--- 08/quiz9/quiz9.js
+++ 08/quiz9/quiz9.js
@@ -2,10 +2,12 @@ const $url = document.querySelector('#url');
 const $btn = document.querySelector('#btn');
 const $log = document.querySelector('#log');
 
+//fetch를 통해 가져온 값 셋팅
 function showup(callback) {
   $log.value = callback;
 }
 
+//fetch
 $btn.addEventListener('click', ()=>{
   let url = $url.value;
   fetch(url)
