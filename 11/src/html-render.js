4af84c1b68612da859decbb9dfecb17e68d2ccce
kang-seongbeom <56289258+kang-seongbeom@users.noreply.github.com>
2021-06-05 20:01:06 +0900

    todos 데이터 local에 저장

--- 11/src/html-render.js
+++ 11/src/html-render.js
@@ -1,7 +1,9 @@
+import { saveData } from './data-manager';
 const $result = document.querySelector('#result');
 
 function render(data) {
   const html = data.map((todo, index) => {
+    saveData(data);
     return `<li data-index="${index}">
       <button class="delete">×</button>
       <input type="checkbox" class="toggle-checked" ${
