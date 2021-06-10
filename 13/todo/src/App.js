38abc5b7148f184df503ac5e6f7149f7e7e52131
kang-seongbeom <56289258+kang-seongbeom@users.noreply.github.com>
2021-06-10 13:13:17 +0900

    count

--- 13/todo/src/App.js
+++ 13/todo/src/App.js
@@ -1,9 +1,16 @@
-
+import React, { useState } from 'react';
+import Header from './Header';
 
 function App() {
+  const [countNumber, setCountNumber] = useState(0);
+  function addCountNumber(){
+    setCountNumber(countNumber + 1);
+  }
   return (
     <div className="App">
-      he
+      <Header/>
+      <div>{countNumber}</div>
+      <button onClick={addCountNumber}>plus</button>
     </div>
   );
 }
